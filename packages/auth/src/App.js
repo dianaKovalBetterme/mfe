afd1629116887287cb4d962fbc0826b4f7b36c50
dianakoval <diana.koval@betterme.world>
2025-01-03 23:40:22 +0200

    webpack dev config updated and updated auth app with markup

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {createGenerateClassName, StylesProvider} from '@material-ui/core/styles';
-import {Router, Switch} from 'react-router-dom';
+import {Route, Router, Switch} from 'react-router-dom';
+import SignIn from './components/Signin';
+import SignUp from './components/Signup';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au',
@@ -12,6 +14,8 @@ export default ({history}) => {
       <StylesProvider generateClassName={generateClassName}>
         <Router history={history}>
           <Switch>
+            <Route path="/auth/signin" component={SignIn}/>
+            <Route path="/auth/signup" component={SignUp}/>
           </Switch>
         </Router>
       </StylesProvider>
